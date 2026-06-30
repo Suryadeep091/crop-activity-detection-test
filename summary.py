d0d8a9c36ac8b26c3ac25a13cc932806c78467eb
Suryadeep091 <suryadeep.singh@azistaindustries.com>
2026-06-30 17:01:52 +0530

    Test 2024

--- summary.py
+++ summary.py
@@ -3,8 +3,8 @@ import os
 
 # --- CONFIGURATION ---
 downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-results_file = os.path.join(downloads_path, "dummy_run_1.json")
-comparison_report = os.path.join(downloads_path, "25-26_analysis.txt")
+results_file = os.path.join(downloads_path, "dummy_run_2.json")
+comparison_report = os.path.join(downloads_path, "24-25_analysis.txt")
 
 def generate_accuracy_report():
     try:
